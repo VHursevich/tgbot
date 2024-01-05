253b7f8429045cd6fb7bc56498283e3a13e4d75d
VadimKhurs <3130021@gmail.com>
2024-01-05 06:03:27 -0500

    ssffs

--- src/bot.mjs
+++ src/bot.mjs
@@ -7,7 +7,7 @@ bot.on("text", msg => msg.text.startsWith('/')?null:msg.reply.text(msg.text));
 
 bot.on('/start', (msg) => {    
     msg.reply.photo('http://thecatapi.com/api/images/get')
-    return bot.sendMessage(msg.from.id, `Здравствуйте, ${ msg.from.first_name }!Для подтверждения вашего ТГ аккаунта нажмите "ДА", если вы не желаете авторизовываться, то нажмите кнопку "НЕТ".Вы в любое время сможете подтвердить ваш аккаунт заново.`);
+    return bot.sendMessage(msg.from.id, `Здравствуfйте, ${ msg.from.first_name }!Для подтверждения вашего ТГ аккаунта нажмите "ДА", если вы не желаете авторизовываться, то нажмите кнопку "НЕТ".Вы в любое время сможете подтвердить ваш аккаунт заново.`);
 });
 
 bot.on("/env", (msg) => msg.reply.text(process.env.VERCEL_ENV));
