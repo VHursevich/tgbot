a47584f10b70565774bf3bbc92ca69da42602160
VadimKhurs <3130021@gmail.com>
2024-01-05 06:17:45 -0500

    ssffs

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mongodb from 'mongodb';
 
 function getMongoClient() {
-  return mongodb.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+  return mongodb.connect(process.env.DB_URL);
 }
 
 module.exports = getMongoClient;
