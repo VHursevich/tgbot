c86d46a27399ea0dc579ddeb20d91efa56f123dd
VadimKhurs <3130021@gmail.com>
2024-01-05 03:54:41 -0500

    sd

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,4 +4,8 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on("text", msg => msg.reply.text(msg.text))
 
+bot.on('/start', (msg) => {    
+    return bot.sendMessage(msg.from.id, `Здравствуйте, ${ msg.from.first_name }!Для подтверждения вашего ТГ аккаунта нажмите "ДА", если вы не желаете авторизовываться, то нажмите кнопку "НЕТ".Вы в любое время сможете подтвердить ваш аккаунт заново.`);
+});
+
 export default bot
