d4b4e759cef6ff9b9ff5b82296b67030fc6791cb
VadimKhurs <3130021@gmail.com>
2024-01-05 08:01:54 -0500

    ssffs

--- src/bot.mjs
+++ src/bot.mjs
@@ -7,11 +7,17 @@ bot.on("text", msg => msg.text.startsWith('/')?null:msg.reply.text(msg.text));
 
 bot.on('/start', (msg) => {    
     msg.reply.photo('http://thecatapi.com/api/images/get')
-    return bot.sendMessage(msg.from.id, `Здравствуfйте, ${ msg.from.first_name }!Для подтверждения вашего ТГ аккаунта нажмите "ДА", если вы не желаете авторизовываться, то нажмите кнопку "НЕТ".Вы в любое время сможете подтвердить ваш аккаунт заново.`);
+    return bot.sendMessage(msg.from.id, `Здравствуйте, ${ msg.from.first_name }!Для подтверждения вашего ТГ аккаунта нажмите "ДА", если вы не желаете авторизовываться, то нажмите кнопку "НЕТ".Вы в любое время сможете подтвердить ваш аккаунт заново.`);
 });
 
 bot.on("/env", (msg) => msg.reply.text(process.env.VERCEL_ENV));
 
 bot.on("/db", (msg) => msg.reply.text(mongo.db().databaseName));
 
+bot.on("/user", async (msg) => msg.reply.text(JSON.stringify(await fetchUsers())));
+
 export default bot
+
+async function fetchUsers(){
+    return await mongo.db('test').collection('users').findOne({email: "3130021@gmail.com"});
+}
