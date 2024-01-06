19665cf2f5a9ec6dcb55af242564482702cec807
VadimKhurs <3130021@gmail.com>
2024-01-06 13:12:58 -0500

    ssffs

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,39 +1,8 @@
 import TeleBot from "telebot"
 import mongo from './db.mjs'
 
-const BUTTONS = {
-    hello: {
-        label: '👋 Hello',
-        command: '/hello'
-    },
-    world: {
-        label: '🌍 World',
-        command: '/world'
-    },
-    hide: {
-        label: '⌨️ Hide keyboard',
-        command: '/hide'
-    }
-};
-
 const bot = new TeleBot({token: process.env.TELEGRAM_BOT_TOKEN,
     usePlugins: ['commandButton']
-});
-
-// Inline buttons
-bot.on('/inlineKeyboard', msg => {
-
-    let replyMarkup = bot.inlineKeyboard([
-        [
-            bot.inlineButton('callback', {callback: 'this_is_data'}),
-            bot.inlineButton('inline', {inline: 'some query'})
-        ], [
-            bot.inlineButton('url', {url: 'https://telegram.org'})
-        ]
-    ]);
-
-    return bot.sendMessage(msg.from.id, 'Inline keyboard example.', {replyMarkup});
-
 });
 
 bot.on("text", msg => msg.text.startsWith('/')?null:msg.reply.text(msg.text));
@@ -62,13 +31,15 @@ bot.on('/hello', msg => {
     return bot.sendMessage(msg.from.id, 'Hello!');
 });
 
-
 bot.on("/env", (msg) => msg.reply.text(process.env.VERCEL_ENV));
 
 bot.on("/db", (msg) => msg.reply.text(mongo.db().databaseName));
 
 bot.on("/user", async (msg) => msg.reply.text(JSON.stringify(await fetchUsers())));
 
+bot.on("/myuser", async (msg) => {
+    return bot.sendMessage(msg.from.id, JSON.stringify(msg.from));
+});
 
 export default bot
 
