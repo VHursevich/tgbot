b4f83ec2168f71f647bbcd568ef6270486f67733
VadimKhurs <3130021@gmail.com>
2024-01-06 13:49:14 -0500

    ssffs

--- src/bot.mjs
+++ src/bot.mjs
@@ -13,7 +13,8 @@ bot.on('/start', msg => {
     const replyMarkup = bot.inlineKeyboard([
         [
             // First row with command callback button
-            bot.inlineButton('Command button', {callback: '/hello'})
+            bot.inlineButton('Command button', {callback: '/hello'}),
+            bot.inlineButton('Regular data button', {callback: 'hello'})
         ],
         [
             // Second row with regular command button
@@ -31,17 +32,13 @@ bot.on('/hello', msg => {
     return bot.sendMessage(msg.from.id, 'Hello!');
 });
 
-bot.on("/env", (msg) => msg.reply.text(process.env.VERCEL_ENV));
-
-bot.on("/db", (msg) => msg.reply.text(mongo.db().databaseName));
-
 bot.on("/user", async (msg) => msg.reply.text(JSON.stringify(await fetchUsers())));
 
 bot.on("/myuser", async (msg) => {
     return bot.sendMessage(msg.from.id, msg.from.username);
 });
 
-export default bot
+export default bot;
 
 async function fetchUsers(){
     return await mongo.db('test').collection('users').findOne({username: "3130021@gmail.com"});
