d8e38469106f9b7c18a4d19adfb19afe70635a5b
VadimKhurs <3130021@gmail.com>
2024-01-07 03:00:10 -0500

    ssffs

--- src/bot.mjs
+++ src/bot.mjs
@@ -5,39 +5,52 @@ const bot = new TeleBot({token: process.env.TELEGRAM_BOT_TOKEN,
     usePlugins: ['commandButton']
 });
 
+// Inline keyboard markup
+const startButtons = bot.inlineKeyboard([
+    [
+        // First row with command callback button
+        bot.inlineButton('Авторизация', {callback: '/askPermission'}),
+    
+        bot.inlineButton('Забыли пароль?', {callback: '/password'})
+    ],
+    [
+        // Second row with regular command button
+        bot.inlineButton('Скажи "Привет"', {callback: '/hello'})
+    ]
+]);
+
+const askPerButtons = bot.inlineKeyboard([
+    [
+        bot.inlineButton('Да, я хочу авторизоваться!', {callback: '/authorization'}),
+    ],
+
+    [
+        bot.inlineButton('Нет, я пока не хочу авторизовываться!', {callback: '/AnswerNo'}),
+    ]
+]);
+
 bot.on("text", msg => msg.text.startsWith('/')?null:msg.reply.text(msg.text));
 
 bot.on('/start', msg => {
-
-    // Inline keyboard markup
-    const replyMarkup = bot.inlineKeyboard([
-        [
-            // First row with command callback button
-            bot.inlineButton('Авторизация', {callback: '/authorization'}),
-        
-            bot.inlineButton('Забыли пароль?', {callback: '/password'})
-        ],
-        [
-            // Second row with regular command button
-            bot.inlineButton('Regular data button', {callback: '/hello'})
-        ]
-    ]);
-
     // Send message with keyboard markup
-    return bot.sendMessage(msg.from.id, 'Example of command button.', {replyMarkup});
+    return bot.sendMessage(msg.from.id, `Здравствуйте, ${msg.from.username}, что вы хотите сделать?`, {startButtons});
 
 });
 
-// Command /hello
-bot.on('/hello', async msg => {
-    return bot.sendMessage(msg.from.id, 'Hello!');
+bot.on("/hello", async (msg) => {
+    return bot.sendMessage(msg.from.id, "Привет!");
+});
+
+bot.on("/askPermission", async (msg) => {
+
+    return bot.sendMessage(msg.from.id, `Проверьте ваш username "${msg.from.username}"\nЕсли username верный и вы желаете авторизовать ваш аккаунт на сайте, то нажмите "Да", в ином случае "Нет"`, {askPerButtons});
+});
+
+bot.on("/AnswerNo", async (msg) => {
+    return bot.sendMessage(msg.from.id, "Если вы захотите авторизоваться или обновить пароль при потере, то обращайтесь.\nХорошего дня!");
 });
 
 bot.on("/user", async (msg) => msg.reply.text(JSON.stringify(await fetchUsers())));
-
-bot.on("/myuser", async (msg) => {
-    return bot.sendMessage(msg.from.id, msg.from.username);
-});
 
 bot.on("/authorization", async (msg) => {
     const user = await mongo.db('test').collection('users').findOne({username: msg.from.username});
@@ -49,9 +62,9 @@ bot.on("/authorization", async (msg) => {
         return bot.sendMessage(msg.from.id, `Вы уже были авторизованы на нашем сайте, наслаждайтесь сочинениями!`);
     }
 
-    await mongo.db('test').collection('users').updateOne({username: msg.from.username}, {$set: {date: new Date()}});
+    await mongo.db('test').collection('users').updateOne({username: msg .from.username}, {$set: {date: new Date()}});
 
-    return bot.sendMessage(msg.from.id, `Пользователь ${msg.from.username} был авторизован`);
+    return bot.sendMessage(msg.from.id, `Пользователь ${msg.from.username} был авторизован.\nНаслаждайтесь сайтом!`);
 });
 
 export default bot;
