aa768c82cf72be364d206078344c5bb98e6fde2c
VadimKhurs <3130021@gmail.com>
2024-01-07 04:43:21 -0500

    ssffs

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,11 +1,13 @@
 import TeleBot from "telebot"
 import mongo from './db.mjs'
+import {ObjectId} from 'mongodb'
 
 const bot = new TeleBot({token: process.env.TELEGRAM_BOT_TOKEN,
     usePlugins: ['commandButton', 'askUser']
 });
 
 bot.on("text", msg => msg.text.startsWith('/')?null:msg.reply.text(msg.text));
+
 
 bot.on('/start', msg => {
     // Inline keyboard markup
@@ -27,9 +29,13 @@ bot.on('/start', msg => {
 
 });
 
+
+
 bot.on("/hello", (msg) => {
     return bot.sendMessage(msg.from.id, "Привет!");
 });
+
+
 
 bot.on("/askPermission", (msg) => {
     const askPerButtons = bot.inlineKeyboard([
@@ -45,9 +51,13 @@ bot.on("/askPermission", (msg) => {
     return bot.sendMessage(msg.from.id, `Проверьте ваш username "${msg.from.username}"\nЕсли username верный и вы желаете авторизовать ваш аккаунт на сайте, то нажмите "Да", в ином случае "Нет"`, {replyMarkup: askPerButtons});
 });
 
+
+
 bot.on("/answerNo", (msg) => {
     return bot.sendMessage(msg.from.id, "Если вы захотите авторизоваться или обновить пароль при потере, то обращайтесь.\nХорошего дня!");
 });
+
+
 
 bot.on("/authorization", async (msg) => {
     const user = await mongo.db('test').collection('users').findOne({username: msg.from.username});
@@ -59,10 +69,12 @@ bot.on("/authorization", async (msg) => {
         return bot.sendMessage(msg.from.id, `Вы уже были авторизованы на нашем сайте, наслаждайтесь сочинениями!`);
     }
 
-    await mong.db('test').collection('users').updateOne({username: msg .from.username}, {$set: {date: new Date()}});
+    await mongo.db('test').collection('users').updateOne({username: msg .from.username}, {$set: {date: new Date()}});
 
     return bot.sendMessage(msg.from.id, `Пользователь ${msg.from.username} был авторизован.\nНаслаждайтесь сайтом!`);
 });
+
+
 
 bot.on("/forgetPass", async (msg) => {
     const changePassButtons = bot.inlineKeyboard([
@@ -78,70 +90,34 @@ bot.on("/forgetPass", async (msg) => {
     return bot.sendMessage(msg.from.id, `Чтобы сменить пароль нажмите "Новый пароль", в противном случае "Я не хочу менять пароль!"`, {replyMarkup: changePassButtons});
 });
 
+
+
 bot.on("/changePass", async (msg) => {
     const user = await mongo.db('test').collection('users').findOne({username: msg.from.username});
 
     if(!user)
         return bot.sendMessage(msg.from.id, `Ваш username: ${msg.from.username}\nДанный username не был зарегистрирован на сайте`);
 
-    if(user.date.getTime() != new Date(0).getTime()){
-        return bot.sendMessage(msg.from.id, `Вы уже были авторизованы на нашем сайте, наслаждайтесь сочинениями!`);
+    return bot.sendMessage(msg.id, 'Введите ваш новый пароль размером 5-32 символов, не забывайте его снова!', {ask: 'password'});
+});
+
+
+
+bot.on('ask.password', async msg => {
+    const newPassword = msg.text;
+
+    if(newPassword.lenght > 32 && newPassword.lenght < 5){
+        return bot.sendMessage(msg.from.id, `Пароль должен содержать 5-32 символов!`);
     }
 
-    await mongo.db('test').collection('users').updateOne({username: msg .from.username}, {$set: {date: new Date()}});
+    const user = await mongo.db('test').collection('users').updateOne({username: msg.from.username}, {$set: {password: newPassword}});
 
-    return bot.sendMessage(msg.from.id, `Пользователь ${msg.from.username} был авторизован.\nНаслаждайтесь сайтом!`);
-});
+    await mongo.db('test').collection('tokens').deleteMany({user: new ObjectId(user._id)});
 
-
-
-
-
-
-
-
-
-
-// On start command
-bot.on('/test', msg => {
-
-    const id = msg.from.id;
-
-    // Ask user name
-    return bot.sendMessage(id, 'What is your name?', {ask: 'name'});
+    return bot.sendMessage(msg.from.id, `Ваш пароль изменён, теперь можете входить в ваш аккаунт с новым паролем!\nХороших вам сочинений`);
 
 });
 
-// Ask name event
-bot.on('ask.name', msg => {
-
-    const id = msg.from.id;
-    const name = msg.text;
-
-    // Ask user age
-    return bot.sendMessage(id, `Nice to meet you, ${ name }! How old are you?`, {ask: 'age'});
-
-});
-
-// Ask age event
-bot.on('ask.age', msg => {
-
-    const id = msg.from.id;
-    const age = Number(msg.text);
-
-    if (!age) {
-
-        // If incorrect age, ask again
-        return bot.sendMessage(id, 'Incorrect age. Please, try again!', {ask: 'age'});
-
-    } else {
-
-        // Last message (don't ask)
-        return bot.sendMessage(id, `You are ${ age } years old. Great!`);
-
-    }
-
-});
 
 
 export default bot;
